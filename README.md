1edb9b285a4bab145953ff3c619750f1287b0754
weison <weisonweileen@gmail.com>
2024-03-06 01:34:39 -0900

    ADD bottle selection and distance computation
    Signed-off-by: weison <weisonweileen@gmail.com>

--- README.md
+++ README.md
@@ -1,4 +1,57 @@
 # YOLO-v8 + Mediapipe to assist the daily life of patients with hemiplegia grasping
-Here I may change the source code of ultralytics to improve the performance of this project.
 
+>Tips: Here I may change the source code of     ultralytics to improve the performance of this project.
+## This is the plotting call stack
+```mermaid
+flowchart TB;
 
+    subgraph distance computing
+        id6
+        id10
+    end
+
+    subgraph yolov8
+    direction TB
+        id1[result]--select bottle-->id2[annotator]-->id3[boxes]--pred_boxes_show_boxes-->id4["box_label() "]-->id5["cv2_rectangle() cv2_circle()"]
+        id3-->id6["box_center"]
+    end
+
+    subgraph mediapipe
+    direction TB;
+        id8[result]-->id9[hand_landmarks]-->id10[index_9 landmark]
+        id9-->id14["cv2.circle()"]
+    end
+    
+```
+Tips:
+- add name property `name_handler` in the Box class for the result extract in the main function, which is assigned in the `Results.plot()`.
+    ```
+    d.name_handler = label
+    ```
+- and the final 2D box center is also assigned in the `Results.plot().annotate.box_label()` function.
+    ```
+    self.center_handler = 0
+    ```
+# The key data format transform about plotting the result: from tensor to array
+from the given markdown description, we can see the `xyxy` is in Tensor format
+| Name      | Type                      | Description                                                        |
+|-----------|---------------------------|--------------------------------------------------------------------|
+| `cpu()`   | Method                    | Move the object to CPU memory.                                     |
+| `numpy()` | Method                    | Convert the object to a numpy array.                               |
+| `cuda()`  | Method                    | Move the object to CUDA memory.                                    |
+| `to()`    | Method                    | Move the object to the specified device.                           |
+| `xyxy`    | Property (`torch.Tensor`) | Return the boxes in xyxy format.                                   |
+| `conf`    | Property (`torch.Tensor`) | Return the confidence values of the boxes.                         |
+| `cls`     | Property (`torch.Tensor`) | Return the class values of the boxes.                              |
+| `id`      | Property (`torch.Tensor`) | Return the track IDs of the boxes (if available).                  |
+| `xywh`    | Property (`torch.Tensor`) | Return the boxes in xywh format.                                   |
+| `xyxyn`   | Property (`torch.Tensor`) | Return the boxes in xyxy format normalized by original image size. |
+| `xywhn`   | Property (`torch.Tensor`) | Return the boxes in xywh format normalized by original image size. |
+
+and then the numpy `squeeze()` function transfer the four detect point into 
+```
+annotator.box_label(d.xyxy.squeeze(), label, color=colors(c, True))
+```
+
+@ TODO
+- 读对面的，记录电刺激参数的帧末尾处理，记录，下次读进来作为，读杯子的宽度
--- combine.py
+++ combine.py
@@ -2,9 +2,12 @@ import cv2
 import mediapipe as mp
 import numpy as np
 from ultralytics import YOLO
+import math
 
 # Load YOLO model
 yolo_model = YOLO('hand_detection/yolov8x.pt')
+
+
 
 #Mediapipe的字体设置
 font = cv2.FONT_HERSHEY_SIMPLEX
@@ -23,7 +26,24 @@ hands_model = mp_hands.Hands(
 
 cap = cv2.VideoCapture(0)
 
+
+
+distance = 0
+
+# hand_position = np.array([0,0])
+# cup_position = np.array([0,0])
+
+#用于记录是否识别到 bottle 和 hand
+box_flag = False
+hand_flag = False
+
 while True:
+
+    hand_position = np.array([0,0])
+    cup_position = np.array([0,0])
+    hand_flag = False
+
+
     ret, frame = cap.read()
     
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -42,13 +62,13 @@ while True:
                     # 索引为0代表手底部中间部位，为4代表手指关键或指尖
                     # print(index, lm)  # 输出21个手部关键点的xyz坐标(0-1之间)，是相对于图像的长宽比例
                     # 只需使用x和y查找位置信息
-                    
+                    yolo_model
                     # 将xy的比例坐标转换成像素坐标
                     h, w, c = frame.shape # 分别存放图像长\宽\通道数
                     
                     # 中心坐标(小数)，必须转换成整数(像素坐标)
                     cx ,cy =  int(lm.x * w), int(lm.y * h) #比例坐标x乘以宽度得像素坐标
-                    
+                    hand_position = np.array([int(lm.x * w),int(lm.y * h)])
                     # 打印显示21个关键点的像素坐标
                     print(index, cx, cy)
                     
@@ -59,12 +79,77 @@ while True:
                     cv2.circle(frame, (cx,cy), 5, (0,0,255), cv2.FILLED)
                     text = f'({cx}, {cy})'
                     cv2.putText(frame, text, (cx, cy - 10), font, font_scale, font_color, font_thickness)
+                    
+
+                    #识别到了，调整flag
+                    hand_flag = True
     # cv2.imshow('MediaPipe Hands', frame)
-    resuzlts = yolo_model(frame, show=True,)
-    print("----------------begin----------------------\n")
-    if resuzlts:
-        print(resuzlts[0].names)
-    print("-----------------end---------------------\n")
+    yolo_results = yolo_model.predict(frame, show=True,)
+    for r in yolo_results:
+        for box in r.boxes:
+            if box.cls.cpu().numpy().astype(int) == 39 and hand_flag == True:  #bottle的id是39
+                four_points = box.xyxy.squeeze()
+                cup_position = np.array([
+                    (int(four_points[2]) - int(four_points[0])) / 2 + int(four_points[0]),
+                    (int(four_points[3]) - int(four_points[1])) / 2 + int(four_points[1])
+                    ])
+                
+                distance = np.linalg.norm(hand_position - cup_position)
+                print("两点之间的距离:", distance)
+                # cv2.line(frame, cup_position, hand_position, (0,0,255), font_thickness)
+                # cv2.line(frame, (hand_position[0], hand_position[1]), (cup_position[0], cup_position[1]), (0, 0, 255), 2)
+                cv2.line(frame, (int(hand_position[0]), int(hand_position[1])), (int(cup_position[0]), int(cup_position[1])), (0, 0, 255), 5)
+                
+                
+
+
+    # for r in yolo_results:
+    #     i = 0
+
+
+
+    # print("----------------begin----------------------\n")
+    # if yolo_results:
+    #     print(yolo_results[0])
+    # print("-----------------end---------------------\n")
+
+
+    # exit_flag = False
+    # #计算模块，我好像懂了，是不是两个results是类，boxes是个数
+    # if yolo_results:
+    #     # for box in yolo_results.pred_boxes:
+    #     for r1 in yolo_results:
+    #         for box in r1.boxes:
+    #             if 'bottle' in box.name_handler:
+    #                 box_flag = True
+    #                 if box_flag and hand_flag:
+    #                     distance = math.sqrt((box.center_handler[0]- hand_position[0])^2
+    #                                         +(box.center_handler[1]- hand_position[1])^2)
+    #                     text = f'the distance is {distance}'
+    #                     print(text)
+    #                     # cv2.putText(frame, text, (cx+20, cy+20), font, font_scale, font_color, font_thickness)
+    #                     cv2.line(frame, box.center_handler, hand_position, (0,0,255), font_thickness)
+                        
+
+    #                     exit_flag = True  # 外面的for循环的设置退出标志，避免多个检测结果
+    #                     break
+    #             if exit_flag:
+                    # break
+    
+
+    #文件之间的通信，失败
+    # if current_cup_detect_flag and hand_flag:
+    #     distance = math.sqrt((current_cup_position[0]- hand_position[0])^2
+    #                 +(current_cup_position[1]- hand_position[1])^2)
+    #     cv2.line(frame, current_cup_position, hand_position, (0,0,255), font_thickness)
+    #     current_cup_detect_flag = False
+    #     hand_flag = False
+    #     #next frame
+        
+
+
+    box_flag = False
+    hand_flag = False
 
         
     if cv2.waitKey(1) & 0xFF == 27:
